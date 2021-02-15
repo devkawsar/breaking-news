9256f3cd0f63714bfae916405009c2291e149429
webdeveloperkawsarahmed <kawsarahmsylhet@gmail.com>
2021-02-15 19:04:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import News from "./components/News/News";
+import Header from "./components/Header/Header";
+import TopHeadline from "./components/TopHeadline/TopHeadline";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-primary">Hello World</h1>
+			<Header></Header>
+			<TopHeadline></TopHeadline>
+			<News></News>
+		</div>
+	);
 }
 
 export default App;
